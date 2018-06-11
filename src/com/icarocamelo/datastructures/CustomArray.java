932087c62ed729f68776f7b59167d3155941ba6d
Icaro Camelo <icarorvc@gmail.com>
2018-06-11 13:35:26 -0400

    Array: implementation and unit tests

--- src/com/icarocamelo/datastructures/CustomArray.java
+++ src/com/icarocamelo/datastructures/CustomArray.java
@@ -3,17 +3,67 @@ package com.icarocamelo.datastructures;
 public class CustomArray {
     String[] values;
     private int capacity;
+    private int size = 0;
 
     public CustomArray(int capacity){
-        this.capacity = capacity;
         values = new String[capacity];
     }
 
-    public int getCapacity() {
+    public int getCapacity(){
+        capacity = values.length;
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
+    public void add(String element) {
+        if (size + 1 > getCapacity()){
+            resize();
+        }
+
+        // insert element
+        values[size] = element;
+
+        // update last element index
+        size++;
+    }
+
+    private void resize() {
+        // resize
+        String[] newArray = new String[getCapacity()*2];
+
+        // copy elements from one array to another
+        for(int i = 0; i < values.length; i++){
+            newArray[i] = values[i];
+        }
+
+        // update reference
+        values = newArray;
+    }
+
+    public String get(int index) {
+        return values[index];
+    }
+
+    public void delete(int index) {
+        for (int i = index; i < size-1; i++) {
+            values[i] = values[i+1];
+        }
+        size--;
+    }
+
+    public void insert(int index, String element) {
+        if (size + 1 > getCapacity()){
+            resize();
+        }
+
+        for (int i = size; i > index; i--) {
+            values[i] = values[i-1];
+        }
+
+        values[index] = element;
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
--- test/com/icarocamelo/datastructures/CustomArrayTests.java
+++ test/com/icarocamelo/datastructures/CustomArrayTests.java
@@ -10,4 +10,100 @@ public class CustomArrayTests {
         CustomArray newArray = new CustomArray(2);
         assertEquals(newArray.getCapacity(), 2);
     }
+
+    @Test
+    public void insertElement(){
+        CustomArray newArray = new CustomArray(3);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+
+        newArray.insert(1, "valueX");
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "valueX");
+        assertEquals(newArray.get(2), "value1");
+        assertEquals(newArray.get(3), "value2");
+    }
+
+    @Test
+    public void addElement(){
+        CustomArray newArray = new CustomArray(2);
+        newArray.add("value0");
+        newArray.add("value1");
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "value1");
+    }
+
+    @Test
+    public void addElementBeyondInitialCapacity(){
+        CustomArray newArray = new CustomArray(2);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "value1");
+        assertEquals(newArray.get(2), "value2");
+        assertEquals(newArray.getCapacity(), 4);
+    }
+
+    @Test
+    public void deleteElement(){
+        CustomArray newArray = new CustomArray(3);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+
+        newArray.delete(1);
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "value2");
+        assertEquals(newArray.getSize(), 2);
+    }
+
+    @Test
+    public void deleteRandomElement(){
+        CustomArray newArray = new CustomArray(4);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+        newArray.add("value3");
+
+        newArray.delete(1);
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "value2");
+        assertEquals(newArray.get(2), "value3");
+        assertEquals(newArray.getSize(), 3);
+    }
+
+    @Test
+    public void deleteFirstElement(){
+        CustomArray newArray = new CustomArray(3);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+
+        newArray.delete(0);
+
+        assertEquals(newArray.get(0), "value1");
+        assertEquals(newArray.get(1), "value2");
+        assertEquals(newArray.getSize(), 2);
+    }
+
+    @Test
+    public void deleteLastElement(){
+        CustomArray newArray = new CustomArray(3);
+        newArray.add("value0");
+        newArray.add("value1");
+        newArray.add("value2");
+
+        newArray.delete(2);
+
+        assertEquals(newArray.get(0), "value0");
+        assertEquals(newArray.get(1), "value1");
+        assertEquals(newArray.getSize(), 2);
+    }
 }
